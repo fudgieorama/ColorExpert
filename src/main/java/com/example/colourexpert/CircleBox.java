c131cc04769662031039dcfe8dfc430231d75931
fudgieorama <w1301785@myfcc.frederick.edu>
2022-11-30 21:39:47 -0500

    Major Project Assignment Part 3 - Added HashMap

--- src/main/java/com/example/colourexpert/CircleBox.java
+++ src/main/java/com/example/colourexpert/CircleBox.java
@@ -1,2 +1,69 @@
-package com.example.colourexpert;public class CircleBox {
+package com.example.colourexpert;
+
+import javafx.scene.Group;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+
+import java.util.HashMap;
+import java.util.Random;
+
+public class CircleBox {
+    private int redScore = 0;
+    private int greenScore = 0;
+    private int blueScore = 0;
+    HashMap<Circle, Integer> colorCounter = new HashMap<>();
+
+    // Getters
+    public int getRedScore() {
+        return redScore;
+    }
+    public int getGreenScore() {
+        return greenScore;
+    }
+    public int getBlueScore() {
+        return blueScore;
+    }
+    public HashMap<Circle, Integer> getColorCounter() {
+        return colorCounter;
+    }
+
+    public void createCircleBox(Group root, int x, int y, int lowerRange, int higherRange) {
+        for (int row=0; row<3; row++) {
+            for (int col=0; col<3; col++) {
+                createCircle(root, x, y, lowerRange, higherRange);
+                x += 20;
+            }
+            x -= 20 * 3;
+            y +=20;
+        }
+    }
+
+    private void createCircle(Group root, double x, double y, int lowerRange, int higherRange) {
+        int score = 0;
+
+        AnchorPane anchorPane = new AnchorPane();
+        Circle c = new Circle(x,y,10);
+        c.setFill(getRandomColor(lowerRange, higherRange));
+        anchorPane.getChildren().add(c);
+        root.getChildren().add(anchorPane);
+        colorCounter.put(c, score);
+    }
+
+    public Color getRandomColor(int lowerRange, int higherRange) {
+        Random rand = new Random();
+        int r = lowerRange + rand.nextInt(higherRange-lowerRange);
+        int g = lowerRange + rand.nextInt(higherRange-lowerRange);
+        int b = lowerRange + rand.nextInt(higherRange-lowerRange);
+        Color color = Color.rgb(r,g,b,1);
+        return color;
+    }
+
+    private boolean determineColorValue(int colorValue) {
+        if (colorValue > 0 && colorValue < 50) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
--- src/main/java/com/example/colourexpert/ColourExpert.java
+++ src/main/java/com/example/colourexpert/ColourExpert.java
@@ -12,8 +12,14 @@ import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Control;
 import javafx.scene.control.Label;
-import javafx.scene.layout.AnchorPane;
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.CycleMethod;
+import javafx.scene.paint.RadialGradient;
+import javafx.scene.paint.Stop;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Shape;
 import javafx.scene.text.Font;
@@ -21,6 +27,7 @@ import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 
 import java.io.*;
+import java.util.Random;
 
 public class ColourExpert extends Application {
 
@@ -28,16 +35,18 @@ public class ColourExpert extends Application {
     private Scene start;
     private Scene about;
     private Scene main;
-
+    private Scene chooseRound;
     private Group root1 = new Group();
     private Group root2 = new Group();
     private Group root3 = new Group();
+    private Group root4 = new Group();
 
-    private ShapeBox<Circle> circleShapeBox = new ShapeBox<>();
-
+    private int rounds = 0;
     private int playerScore = 0;
-
     private int bestScore;
+
+    private boolean playerStartedGame = false;
+    private boolean gameOver = false;
 
     public int canvasWidth = 500;
     public int canvasHeight = 500;
@@ -53,50 +62,123 @@ public class ColourExpert extends Application {
         stage.setResizable(false); // User cannot resize the window
         stage.setTitle("ColourExpert");
 
+
         start = createStart(); // Creates the "start" scene
         about = createAbout(); // Creates the "about" scene
         main = createMain(); // Creates the main scene
+        chooseRound = createChooseRound();
 
-        stage.setScene(main); // The first scene to show when the program runs
+        CircleBox c = new CircleBox();
+        start.setFill(new RadialGradient(
+                0, 0, 0, 0, 1, true,   //sizing
+                CycleMethod.NO_CYCLE,                       //cycling
+                new Stop(0, c.getRandomColor(0,256)),    //colors (in hexadecimal)
+                new Stop(1, c.getRandomColor(0,256)))
+        );
+
+        stage.setScene(chooseRound); // The first scene to show when the program runs
         stage.show();
     }
 
+    private Scene createChooseRound() {
+        Button btn1 = new Button("5 rounds");
+        Button btn2 = new Button("10 rounds");
+        Button btn3 = new Button("20 rounds");
+        Shape Circle = new Circle(6);
+        btn1.setShape(Circle);
+        btn2.setShape(Circle);
+        btn3.setShape(Circle);
+        btn1.setPrefSize(100,50);
+        btn2.setPrefSize(100,50);
+        btn3.setPrefSize(100,50);
+        setLayout(btn1, 200, 150);
+        setLayout(btn2, 200, 200);
+        setLayout(btn3, 200, 250);
+
+        btn1.setOnAction(e -> {
+            setUpGame(5);
+            System.out.println(rounds);
+            switchScenes(main);
+        });
+
+        root4.getChildren().addAll(btn1, btn2, btn3);
+        chooseRound = new Scene(root4, canvasWidth, canvasHeight);
+        chooseRound.setFill(Color.BLACK);
+        return chooseRound;
+    }
+
     private Scene createMain() {
+        Button btn1 = new Button("LEFT");
+        Button btn2 = new Button("RIGHT");
+        setLayout(btn1, 100, 350);
+        setLayout(btn2, 350, 350);
 
-        // Stores the player's score by writing to a file
-        try {
-            BufferedWriter writer = new BufferedWriter(new FileWriter("score.txt"));
-            writer.write(playerScore);
-            writer.close();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        newComparison(200);
 
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader("score.txt"));
-            System.out.println(reader.readLine());
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+//        int i = 0;
+//        while (playerStartedGame) {
+//            newComparison(200);
+//            System.out.println(rounds);
+//        }
 
-
-        AnchorPane anchorPane = new AnchorPane();
-        Circle circle = new Circle(1);
-        circle.setLayoutX(50);
-        circle.setLayoutY(50);
-        anchorPane.getChildren().add(circle);
-        root1.getChildren().add(anchorPane);
+        root1.getChildren().addAll(btn1, btn2);
         main = new Scene(root1, canvasWidth, canvasHeight);
+        main.setFill(Color.BLACK);
         return main;
     }
 
-    private Scene createStart() {
+    public void setUpGame(int num) {
+        playerStartedGame = true;
+        rounds = num;
+    }
+
+    public void increment(int i) {
+        i++;
+    }
+
+    public void setLayout(Control obj, int x, int y) {
+        obj.setLayoutX(x);
+        obj.setLayoutY(y);
+    }
+
+    // Creates two new game boxes
+    public void newComparison(int difference) {
+        int higherRange = getHigherRange(difference);
+        CircleBox c = new CircleBox();
+        c.createCircleBox(root1, 100, 200, getLowerRange(difference, higherRange), higherRange);
+        System.out.println(c.getColorCounter());
+        c.createCircleBox(root1, 350, 200, getLowerRange(difference, higherRange), higherRange);
+        System.out.println(c.getColorCounter());
+    }
+
+    public int getHigherRange(int difference) {
+        Random rand = new Random();
+        int num = rand.nextInt(256);
+        if (num <= difference) {
+            while (num <= difference) {
+                num++;
+            }
+            System.out.println(num);
+            return num;
+        } else if (num > difference) {
+            System.out.println(num);
+            return num;
+        } else {
+            return 256;
+        }
+    }
+
+    public int getLowerRange(int difference, int higherRange) {
+        int num = higherRange - difference;
+        return num;
+    }
+
+    private Scene createStart() throws FileNotFoundException {
 
         // Labels
         Label lbl1 = new Label("Color Expert");
         lbl1.setFont(Font.font(40));
-        lbl1.setLayoutX(140);
-        lbl1.setLayoutY(140);
+        setLayout(lbl1, 140, 140);
 
 
         // Button
@@ -132,26 +214,43 @@ public class ColourExpert extends Application {
                 "                            Ready? Hit the button below!");
 
         lbl1.setFont(Font.font(30));
-        lbl1.setLayoutX(80);
-        lbl1.setLayoutY(60);
-        lbl2.setLayoutX(80);
-        lbl2.setLayoutY(120);
+        setLayout(lbl1, 80, 60);
+        setLayout(lbl2, 80, 120);
+        setLayout(lbl3, 70, 200);
+        lbl1.setTextFill(Color.WHITE);
+        lbl2.setTextFill(Color.WHITE);
+        lbl3.setTextFill(Color.WHITE);
         lbl2.setTextAlignment(TextAlignment.CENTER);
-        lbl3.setLayoutX(70);
-        lbl3.setLayoutY(200);
 
         // Button
         // Buttons to switch scenes
         Button btn1 = new Button("Let's Play!");
-        btn1.setLayoutX(215);
-        btn1.setLayoutY(380);
+        setLayout(btn1,215,380);
         btn1.setOnAction(e -> switchScenes(main));
 
-        root2.getChildren().addAll(lbl1, lbl2, lbl3);
-        root2.getChildren().add(btn1);
+        root2.getChildren().addAll(btn1, lbl1, lbl2, lbl3);
         about = new Scene(root2, canvasWidth, canvasHeight);
+        about.setFill(Color.BLACK);
 
         return about;
+    }
+
+    // Stores the player's score by writing to a file
+    private void storePlayerScore(int score) {
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter("score.txt"));
+            writer.write(score);
+            writer.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("score.txt"));
+            System.out.println(reader.readLine());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void switchScenes(Scene scene) {
